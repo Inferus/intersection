f2614ce1c6343f94e17cf151b9c3a6797c099712
Moonlization <seva2409@gmail.com>
2022-11-26 15:27:11 +0100

    mq and loader

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const postsList = document.querySelector('.posts')
-
+let loading = false
 let newPosts = []
 // "userId": 1,
 //   "id": 1,
@@ -8,6 +8,7 @@ let newPosts = []
 let loadedPosts = 0
 async function loadmorePosts(){
     if (loadedPosts > 96) return
+    loading = true
     for (let i = loadedPosts + 1; i<loadedPosts+4; i++){
        await fetch(`https://jsonplaceholder.typicode.com/posts/${i}`)
         .then(response => response.json())
@@ -30,22 +31,32 @@ function makeNodesFromPosts(){
     postElement.append(postTitle,postBody)
     postElement.classList.add('post')
     postsList.append(postElement)
+    postsList.childNodes.forEach(e=> e.classList?.remove('lastPost'))
     if (i === newPosts.length-1){
         console.log(postsList.childNodes)
-        postsList.childNodes.forEach(e=> e.classList?.remove('lastPost'))
         postElement.classList.add('lastPost')
         const lastPost = document.querySelector('.lastPost')
         postObserver.observe(lastPost)
     }
    })
+   loading = false
 }
 
 loadmorePosts()
 
+const loader = document.createElement('h1')
+loader.classList.add('loader')
 const postObserver = new IntersectionObserver((obs)=>{
     obs.forEach((e)=>{
        if (e.isIntersecting) loadmorePosts()
+       if (loading){
+        loader.innerText = 'Loading posts...'
+        postsList.append(loader)}
     })
+        if (!loading && postsList.contains(loader)){
+            postsList.removeChild(loader)
+            
+        }
 },{
     threshold:1
 })
